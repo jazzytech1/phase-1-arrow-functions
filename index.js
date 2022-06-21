fc375c2dfcf268c9708c581347480111604af1e6
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-06-21 12:07:34 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function divide(){
+return 2000/100
+}
+let x = 2;
+const square = (x) => x ** 2
+
+let a = 3
+let b = 4
+const add = (a,b) => a+b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const divide = (d) => 2000 / 100;
+
+let x = 2;
+const square = (x) => x ** 2;
+
+let a = 3;
+let b = 4;
+const add = (a, b) => a + b; */
